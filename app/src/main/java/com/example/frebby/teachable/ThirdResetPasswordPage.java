a99440a2fd211b66798a65d03102424c216d2814
Frebby98 <fredudoh33@gmail.com>
2019-01-31 09:50:15 +0100

    N.M.F.N

---
+++ app/src/main/java/com/example/frebby/teachable/ThirdResetPasswordPage.java
@@ -0,0 +1,13 @@
+package com.example.frebby.teachable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdResetPasswordPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_reset_password_page);
+    }
+}
